f38a88bb1a9be924e2a1f48120e60be5bba16452
sonurborg <j.medina11@ufromail.cl>
2023-06-15 05:40:08 -0400

    Added loading to payment verification page and fixed state message

--- src/client/pages/PaymentVerified.jsx
+++ src/client/pages/PaymentVerified.jsx
@@ -7,6 +7,7 @@ import {
   Typography,
   Grid,
   Button,
+  CircularProgress,
 } from "@mui/material";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import Footer from "../components/Footer";
@@ -15,6 +16,7 @@ import PaymentApiRest from "../services/PaymentApiRest";
 
 const PaymentVerificationPage = () => {
   const [data, setData] = useState("");
+  const [loading, setLoading] = useState(true);
   const isPortrait = window.matchMedia("(orientation: portrait)").matches;
 
   useEffect(() => {
@@ -27,6 +29,8 @@ const PaymentVerificationPage = () => {
     PaymentApiRest.getPayment(token)
       .then((response) => {
         setData(response);
+        console.log(response);
+        setLoading(false);
       })
       .catch((error) => {
         setData(error.response.data);
@@ -54,51 +58,79 @@ const PaymentVerificationPage = () => {
           }}
         >
           <CardContent>
-            <Grid
-              container
-              spacing={2}
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Grid item xs={12} sm={8}>
-                <Typography variant="h5" gutterBottom textAlign="center">
-                  {data.message !== "Mensaje exitoso del backend"
-                    ? "Ups!, algo salió mal"
-                    : "Pago Existoso!"}
-                </Typography>
-                <Typography variant="body1" textAlign="center">
-                  {data.message !== "Mensaje exitoso del backend"
-                    ? data.message
-                    : "Su pago ha sido realizado con éxito, en breve recibirá un correo con los detalles de su compra y "}
-                </Typography>
-                <Grid item display="flex" justifyContent="center" margin="2rem">
-                  <Link to="/">
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      sx={{ padding: ".5rem 2rem" }}
-                    >
-                      OK!
-                    </Button>
-                  </Link>
+            {loading ? (
+              <Grid container alignItems="center" justifyContent="center">
+                <Grid item xs={12}>
+                  {/* <Box
+                    style={{
+                      flex: 1,
+                      height: "90vh",
+                      width: "100%",
+
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  > */}
+                  <CircularProgress color="secondary" size={90} />
+                  {/* </Box> */}
                 </Grid>
               </Grid>
+            ) : (
               <Grid
-                item
-                xs={12}
-                sm={4}
-                sx={{ display: "flex", justifyContent: "center" }}
+                container
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
               >
-                <img
-                  src="/VerifiedPayment.png"
-                  style={{
-                    marginRight: "0.5rem",
-                    width: "100%",
-                  }}
-                  alt="Verified Mail"
-                />
+                <>
+                  <Grid item xs={12} sm={8}>
+                    <Typography variant="h5" gutterBottom textAlign="center">
+                      {data !== "AUTHORIZED"
+                        ? "Ups!, algo salió mal"
+                        : "Pago Existoso!"}
+                    </Typography>
+                    <Typography variant="body1" textAlign="center">
+                      {data !== "AUTHORIZED"
+                        ? data.message
+                        : "Su pago ha sido realizado con éxito, en breve recibirá un correo con los detalles de su compra y boleto"}
+                    </Typography>
+                    <Grid
+                      item
+                      display="flex"
+                      justifyContent="center"
+                      margin="2rem"
+                    >
+                      <Link to="/">
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          sx={{ padding: ".5rem 2rem" }}
+                        >
+                          OK!
+                        </Button>
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </>
+
+                <Grid
+                  item
+                  xs={12}
+                  sm={4}
+                  sx={{ display: "flex", justifyContent: "center" }}
+                >
+                  <img
+                    src="/VerifiedPayment.png"
+                    style={{
+                      marginRight: "0.5rem",
+                      width: "100%",
+                    }}
+                    alt="Verified Mail"
+                  />
+                </Grid>
               </Grid>
-            </Grid>
+            )}
           </CardContent>
         </Card>
       </Box>
