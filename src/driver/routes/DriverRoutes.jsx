2455471c0f04b4207dc8cce197cecf6ebd030c5e
MaverickCl <dgarrrido.uribe@gmail.com>
2023-07-01 17:20:42 -0400

    routes fixed

--- src/driver/routes/DriverRoutes.jsx
+++ src/driver/routes/DriverRoutes.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import TicketReadingPage from "../pages/TicketReadingPage";
+import { Itinerario } from "../pages/Itinerario";
 
 export const DriverRoutes = () => {
   return (
     <Routes>
       <Route path="/leer-qr" element={<TicketReadingPage />} />
 
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
+      <Route path="/*" element={<Itinerario/>} />
     </Routes>
   );
 };
